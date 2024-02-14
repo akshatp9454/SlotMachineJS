fa69e24eb9c2541ea44f41b862e68c844eb400f7
Akshat Verma <akshatp9454@gmail.com>
2024-02-14 14:26:43 +0530

    getNumberOfLines function implemented

--- project.js
+++ project.js
@@ -21,5 +21,18 @@ const deposit = () => {
     }
 };
 
+const getNumberOfLines = () => {
+    while (true) {
+        const lines = prompt("Enter a number of lines to bet on (1-3): ");
+        const numberOfLines = parseFloat(lines);
+
+        if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+            console.log("Invalid number of lines, try again.");
+        } else {
+            return numberOfLines;
+        }
+    }
+};
+
 const depositAmount = deposit();
-console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
